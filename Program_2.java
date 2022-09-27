d85b176a82e05f310767835348063737e9cdfab9
Manohar0508 <114403077+Manohar0508@users.noreply.github.com>
2022-09-27 10:30:31 +0530

    Add files via upload

---
+++ Program_2.java
@@ -0,0 +1,14 @@
+package programme.array;
+
+import java.util.Scanner;
+public class Program_2{
+	public static void main(String[]args){
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the value");
+		int a=sc.nextInt();
+		for(int i=1; i<=((a+a)-1); i++){
+			if((i%2)==1)
+				System.out.print(i+" ");		
+		}
+	}
+}
