4619c8745025e83984461c411a56bdbbe4cb5aa5
Manohar0508 <114403077+Manohar0508@users.noreply.github.com>
2022-09-27 10:01:39 +0530

    Add files via upload

---
+++ Program_4.py
@@ -0,0 +1,34 @@
+n=int(input("Input a number "))
+d = dict()
+
+for x in range(1,n+1):
+    d[x]=x*x
+
+print(d) 
+
+
+'''
+Input a number 1
+{1: 1}
+
+
+Input a number 1
+{1: 1}
+
+
+
+Input a number 8
+{1: 1, 2: 4, 3: 9, 4: 16, 5: 25, 6: 36, 7: 49, 8: 64}
+
+
+
+Input a number 9
+{1: 1, 2: 4, 3: 9, 4: 16, 5: 25, 6: 36, 7: 49, 8: 64, 9: 81}
+
+
+
+Input a number 12
+{1: 1, 2: 4, 3: 9, 4: 16, 5: 25, 6: 36, 7: 49, 8: 64, 9: 81, 10: 100, 11: 121, 12: 144}
+
+
+'''
